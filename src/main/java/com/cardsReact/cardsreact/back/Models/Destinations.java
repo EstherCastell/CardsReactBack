35471ab4eef3a2766c62a05c01d4e5d695c7e2c1
EstherCastell <esthercastell2014@gmail.com>
2022-03-13 21:35:17 +0100

    destination model added

---
+++ src/main/java/com/cardsReact/cardsreact/back/Models/Destinations.java
@@ -0,0 +1,23 @@
+package com.cardsReact.cardsreact.back.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Destinations {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String title;
+}
