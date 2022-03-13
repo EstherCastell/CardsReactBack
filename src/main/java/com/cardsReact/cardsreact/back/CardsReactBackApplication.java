68a486df14d7bddf3409493c3d15552339fc48ea
EstherCastell <esthercastell2014@gmail.com>
2022-03-13 21:21:55 +0100

    first commit

---
+++ src/main/java/com/cardsReact/cardsreact/back/CardsReactBackApplication.java
@@ -0,0 +1,13 @@
+package com.cardsReact.cardsreact.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsReactBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsReactBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cardsReact/cardsreact/back/CardsReactBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardsReact.cardsreact.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsReactBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
